ac4360e5d077edd89df2a82657e9467ee96d8375
Hemaant-3 <hemantchaudhary4321@gmail.com>
2024-05-07 17:32:38 +0530

    yes

---
+++ practice/bintodec.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void decToBin(int n)
+{
+
+    int sum = 0;
+    int place = 1;
+    while(n){
+    int digit = n%2;
+    // cout<<digit<<" ";
+    sum = sum+place*digit;
+    place = place*10; 
+    n = n/2;
+    }
+    cout<<sum<<endl;
+}
+
+int main()
+{
+int n = 21;
+decToBin(n);
+    return 0;
+}
